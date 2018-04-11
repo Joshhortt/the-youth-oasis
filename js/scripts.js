a29a2088adcd8552fb5fcd5f59a4e1adcc22e200
[joshhortt@yahoo.com] <joshhortt@yahoo.com>
2018-04-11 17:10:35 +0100

    add javascript to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,22 @@ $(document).ready(function() {
     alert("This is a header.");
   });
 
+  $("h2").click(function() {
+    alert("This is a medium header.");
+  });
+
+  $("ul").click(function() {
+    alert("This is unordered list.");
+  });
   $("p").click(function() {
     alert("This is a paragraph.");
+  });
+
+  $("blockquote").click(function() {
+    alert("This is blockquote.");
   });
 
   $("img").click(function() {
     alert("This is an image.");
   });
-  });
+});
